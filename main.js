3901e2a72f80117c65dbaf174a69f4bad12450a7
Fabrice Hafashimana <feytoninc@hotmail.com>
2021-12-03 09:50:25 +0200

    Moved the files to the recommended directory

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function (e) {
+  $header = $(".menu-items");
+
+  $(".menu-toggle").click(function (e) {
+    $header.toggleClass("menu-displayed");
+  });
+  $(window).on("load resize", function () {
+    //to toggle class when user click and left it there
+    $(".menu-items").removeClass("menu-displayed", $(window).width() > 575);
+  });
+});
