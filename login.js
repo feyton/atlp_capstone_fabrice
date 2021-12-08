569bd926214ce5be1d4d240f99290118b196a5ee
Fabrice Hafashimana <feytoninc@hotmail.com>
2021-12-08 22:19:37 +0200

    Valid signup form for all fields
    I added the ability to fecth data after validating form
    This will be useful in firebase project which is next

--- login.js
+++ login.js
@@ -1,25 +1,41 @@
-var inputs = document.querySelectorAll(".input");
+$(document).ready(() => {
+  var inputs = document.querySelectorAll(".input");
 
-function focusFunc() {
-  let parent = this.parentNode.parentNode;
-  parent.classList.add("focus");
-}
-
-function blurFunc() {
-  let parent = this.parentNode.parentNode;
-  if (this.value == "") {
-    parent.classList.remove("focus");
-  }
-}
-function changeFunc() {
-  let parent = this.parentNode.parentNode;
-  if (this.value != "") {
+  function focusFunc() {
+    let parent = this.parentNode.parentNode;
     parent.classList.add("focus");
   }
-}
 
-inputs.forEach((input) => {
-  input.addEventListener("focus", focusFunc);
-  input.addEventListener("blur", blurFunc);
-  input.addEventListener("change", changeFunc);
+  function blurFunc() {
+    let parent = this.parentNode.parentNode;
+    if (this.value == "") {
+      parent.classList.remove("focus");
+    }
+  }
+  function changeFunc() {
+    let parent = this.parentNode.parentNode;
+    if (this.value != "") {
+      parent.classList.add("focus");
+    }
+  }
+
+  inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+    input.addEventListener("change", changeFunc);
+  });
+
+  var checkForm = () => {
+    var email = $("#user-email").val();
+    var password = $("#user-password").val();
+
+    if (email != null && password != null) {
+      console.log(email, password);
+    }
+  };
+
+  $("#login-form").submit((e) => {
+    e.preventDefault();
+    checkForm();
+  });
 });
--- main.js
+++ main.js
@@ -9,9 +9,23 @@ $(document).ready(function (e) {
     $(".menu-items").removeClass("menu-displayed", $(window).width() > 575);
   });
 
-
   // Disabling links and buttons
   $(".disabled").click(function (e) {
     e.preventDefault();
   });
+
+  var setNav = () => {
+    var path = window.location.pathname;
+    path = decodeURIComponent(path);
+    $(".menu-items li a").each(() => {
+      var href = $(this).attr("href");
+      console.log(href);
+      if (path.substring(0, href.length) === href && href.length > 1) {
+        $(this).closest("li").addClass("active");
+      } else if (window.location.pathname == "/") {
+        $("menu-home").addClass("active");
+      }
+    });
+  };
+  // setNav();
 });
