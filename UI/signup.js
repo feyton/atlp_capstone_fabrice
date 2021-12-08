641ab2ca335f477ebf79819438b9e9af98186e6f
Fabrice Hafashimana <feytoninc@hotmail.com>
2021-12-08 22:19:37 +0200

    Valid signup form for all fields
    I added the ability to fecth data after validating form
    This will be useful in firebase project which is next

--- UI/signup.js
+++ UI/signup.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  $confInput = $("#user-password-confirm");
   $(".input").each(function () {
     $(this).on("focus", function () {
       //   alert("focused");
@@ -9,12 +10,34 @@ $(document).ready(function () {
     });
   });
 
+  // Email Validation
+  var emailValidation = () => {
+    $email = $("#user-email");
+    // var emailValid = false;
+    var emailRegex = /\S+@\S+\.\S+/;
+    $email.on("keyup", () => {
+      if ($email.val().match(emailRegex)) {
+        $("#email-icon").addClass("valid");
+        $("#email-valid").removeClass("invalid");
+        $("#email-valid").addClass("valid");
+        console.log("valid");
+        // emailValid = true;
+      } else {
+        $("#email-icon").addClass("invalid");
+        $("#email-valid").addClass("invalid");
+        $("#email-valid").removeClass("valid");
+        console.log("invalid");
+      }
+      // return emailValid;
+    });
+  };
+
   //   Form validation
-  function validatePassword() {
+  var validatePassword = () => {
     var lowerCaseLetter = /[a-z]/g;
     var upperCaseLetter = /[A-Z]/g;
     var numbers = /[0-9]/g;
-    var valid = false;
+    // var passwordValid = false;
     $("#user-password").on("keyup", function () {
       $("#password-icon").addClass("invalid");
       //   console.log($(this).val());
@@ -39,33 +62,101 @@ $(document).ready(function () {
         $("#number").removeClass("valid");
         $("#number").addClass("invalid");
       }
-      if ($(this).val().length >= 6) {
-      }
+
       if (
         $(this).val().match(lowerCaseLetter) &&
         $(this).val().match(upperCaseLetter) &&
         $(this).val().match(numbers) &&
         $(this).val().length >= 6
       ) {
-        valid = true;
         $("#password-icon").removeClass("invalid");
+        $confInput.prop("disabled", false);
+        // return true;
+      } else {
+        // return false;
       }
-      console.log(valid);
-      return valid;
     });
-  }
-  var passVal = validatePassword();
+  };
   function validatePasswordConfirm() {
     $confInput = $("#user-password-confirm");
     $pass = $("#user-password").val();
 
     $confInput.on("keyup", function () {
-      if ($pass == 0) {
-        console.log($confInput.val());
-        $confInput.parent().next(".message-div").innerHTML +=
-          "<h3>Fill Password first</h3>";
+      if ($("#user-password").val() == $(this).val()) {
+        // console.log($(this).val());
+        // confirmPasswordValid = true;
+        $("#password-confirm-icon").addClass("valid");
+        $("#confirm-password-list").addClass("valid");
+        $("#confirm-password-list").removeClass("invalid");
+        $("#form-submit").prop("disabled", false);
+        // return true;
+      } else {
+        console.log($(this).val());
+        console.log($("#user-password").val());
+        $("#password-confirm-icon").addClass("invalid");
+        $("#password-confirm-icon").removeClass("valid");
+        $("#confirm-password-list").addClass("invalid");
+        $("#confirm-password-list").removeClass("valid");
+        // return false;
       }
     });
   }
+  validatePassword();
   validatePasswordConfirm();
+  emailValidation();
+
+  function checkPass(pass) {
+    var lowerCaseLetter = /[a-z]/g;
+    var upperCaseLetter = /[A-Z]/g;
+    var numbers = /[0-9]/g;
+    if (
+      pass.match(lowerCaseLetter) &&
+      pass.match(upperCaseLetter) &&
+      pass.match(numbers)
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  function checkEmail(email) {
+    var emailRegex = /\S+@\S+\.\S+/;
+    if (email.match(emailRegex)) {
+      return true;
+    }
+    return false;
+  }
+
+  var validateSignUpForm = () => {
+    var password = $("#user-password").val();
+    var email = $("#user-email").val();
+    var name = $("#user-name").val();
+
+    if (checkEmail(email) && checkPass(password) && name.length >= 3) {
+      console.log("Form is valid");
+      submitData(name, email, password);
+      return true;
+    } else {
+      console.log("Invalid form");
+      return false;
+    }
+  };
+
+  // Form Submission function
+
+  var submitData = (name, email, password) => {
+    console.log(name, email, password);
+  };
+
+  $("#form").on("submit", function (e) {
+    e.preventDefault();
+    var valid = validateSignUpForm();
+
+    if (valid) {
+      console.log("Valid");
+    } else {
+      alert("Form is not valid");
+      e.preventDefault();
+    }
+  });
 });
