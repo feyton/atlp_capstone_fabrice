6634c3cc46f977a6b1c8c09888e39846f7c45506
Fabrice Hafashimana <feytoninc@hotmail.com>
2021-12-25 21:38:18 +0200

    Folder restructuring
    I managed to restructure the directory tree to be clean.

--- js/main.js
+++ js/main.js
@@ -32,35 +32,15 @@ $(document).ready(function (e) {
     e.preventDefault();
   });
 
-  var setNav = () => {
-    var path = window.location.pathname;
-    path = decodeURIComponent(path);
-    $(".menu-items li a").each((e) => {
-      var href = e.target.getAttribute("href");
-      console.log(href);
-      if (path.substring(0, href.length) === href && href.length > 1) {
-        $(this).closest("li").addClass("active");
-      } else if (
-        window.location.pathname == "/" ||
-        window.location.pathname == "index.html"
-      ) {
-        $("menu-home").addClass("active");
-      }
-    });
-  };
-  // setNav();
-
   // Managing the dropdown on the about me page
 
   $(".drop-menu-item").on("click", (e) => {
     e.preventDefault();
+    let targetID = e.target.getAttribute("data-target");
+    let itemId = e.target.getAttribute("id");
     $(".about-info").addClass("d-none");
     $(".drop-menu-item").removeClass("active");
-    let itemId = e.target.getAttribute("id");
-    $(itemId).addClass("active");
     document.getElementById(itemId).classList.add("active");
-    let targetID = e.target.getAttribute("data-target");
     $(targetID).removeClass("d-none");
-    console.log(targetID);
   });
 });
