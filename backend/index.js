a85e5b4852a64b425275ac45dc9e919bbea8e02e
Fabrice Hafashimana <feytoninc@hotmail.com>
2021-12-27 23:14:01 +0200

    Serving the files and static using express

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,58 @@
-const logEvents = require("./log");
-const http = require("http");
+const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const fsPromises = require("fs").promises;
 
-const EventEmitter = require("events");
-class Emitter extends EventEmitter {}
-
-const myEmitter = new Emitter();
-myEmitter.on("log", (msg) => logEvents(msg));
-
-// setTimeout(() => {
-//   myEmitter.emit("log", "Log event emitted new");
-// }, 2000);
+const app = express();
 
 const PORT = process.env.PORT || 3500;
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(path.dirname(__dirname), "UI/public")));
+
+app.get("^/$|/index(.html)?", (req, res) => {
+  res.sendFile("./index.html", {
+    root: path.join((path.dirname(__dirname), "UI")),
+  });
+  //   console.log(path.dirname(__dirname));
+});
+app.get("/pages/about(.html)?|/about(.html)?", (req, res) => {
+  res.sendFile("./pages/about.html", {
+    root: path.join((path.dirname(__dirname), "UI")),
+  });
+  //   console.log(path.dirname(__dirname));
+});
+app.get("/pages/blog(.html)?|/blog(.html)?", (req, res) => {
+  res.sendFile("./pages/blog.html", {
+    root: path.join((path.dirname(__dirname), "UI")),
+  });
+  //   console.log(path.dirname(__dirname));
+});
+app.get("/pages/detail(.html)?|/blog/detail(.html)?", (req, res) => {
+  res.sendFile("./pages/detail.html", {
+    root: path.join((path.dirname(__dirname), "UI")),
+  });
+  //   console.log(path.dirname(__dirname));
 });
 
-server.listen(PORT, () => {
+// Route Handlers
+
+// app.get(
+//   "/blog(.html)?",
+//   (req, res, next) => {
+//     console.log("Hello there");
+//     next();
+//   },
+//   (req, res) => {
+//     res.send("testing");  //Testing the use of next like in login and next
+//   }
+// );
+
+// Handling 404
+app.get("/*", (req, res) => {
+  res.status(404).send("Page not found");
+});
+app.listen(PORT, () => {
   console.log("Server listening on " + PORT);
 });
--- backend/log.js
+++ backend/log.js
@@ -27,3 +27,13 @@ const logEvents = async (message) => {
 };
 
 module.exports = logEvents;
+
+const EventEmitter = require("events");
+class Emitter extends EventEmitter {}
+
+const myEmitter = new Emitter();
+myEmitter.on("log", (msg) => logEvents(msg));
+
+// setTimeout(() => {
+//   myEmitter.emit("log", "Log event emitted new");
+// }, 2000);
