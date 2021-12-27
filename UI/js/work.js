6ee7073cfb4a5b0b821877ee251ad51fac03a0bb
Fabrice Hafashimana <feytoninc@hotmail.com>
2021-12-27 20:35:34 +0200

    Fixed the user profile and minor styling
    Fixed the toggle of user when updating profile
    Added the box-shadow on post details

--- UI/js/work.js
+++ UI/js/work.js
@@ -2,9 +2,7 @@ let modalButtons = document.querySelectorAll("button.modal-view");
 modalButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("Clicked");
     let dataTarget = e.target.getAttribute("data-target");
-    console.log(dataTarget);
     let dataModal = document.getElementById(dataTarget);
     dataModal.style.display = "block";
   });
