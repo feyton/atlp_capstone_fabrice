30f09766de28fc432bd5a516d6747a085402aafa
Fabrice Hafashimana <feytoninc@hotmail.com>
2021-12-27 23:37:57 +0200

    Added a CORS whitelist

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const { logEvents, logger } = require("./log");
 const fsPromises = require("fs").promises;
+const cors = require("cors");
 
 const app = express();
 
@@ -10,6 +11,25 @@ const PORT = process.env.PORT || 3500;
 
 // Custom middleware
 app.use(logger);
+const whiteList = [
+  "https://www.feyton.co.rw",
+  "http://127.0.0.1:5500",
+  "http://127.0.0.1:3500",
+];
+
+// Using cors to allow access
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
