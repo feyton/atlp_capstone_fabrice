8e590cfde02253a19ec25929847991a8b9666350
Fabrice Hafashimana <feytoninc@hotmail.com>
2021-12-27 20:35:34 +0200

    Fixed the user profile and minor styling
    Fixed the toggle of user when updating profile
    Added the box-shadow on post details

--- js/profile.js
+++ js/profile.js
@@ -1,4 +1,25 @@
-$(".btn-update-profile").click((e) => {
-  e.preventDefault();
-  $(".update-div").toggle(200);
+$(document).ready(() => {
+  $(".btn-update-profile").click((e) => {
+    e.preventDefault();
+    $(".update-div").toggle(200);
+  });
+
+  $(".modal #hideModal").click((e) => {
+    e.preventDefault();
+    $(".modal").toggle(200);
+  });
+  $(".update-picture").click((e) => {
+    e.preventDefault();
+    $(".modal").toggle(200);
+  });
+
+  let image = $("#user-image");
+  image.on("change", () => {
+    let file = image[0].files[0];
+    let reader = new FileReader();
+    reader.onload = (e) => {
+      $(".image-div").html(`<img src="${e.target.result}" alt="">`);
+    };
+    reader.readAsDataURL(file);
+  });
 });
