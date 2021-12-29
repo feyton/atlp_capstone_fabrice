7a794fbc290d844da82f72a0f4c7ddf23c0e6ed3
Fabrice Hafashimana <feytoninc@hotmail.com>
2021-12-29 22:36:52 +0200

    Fixed the connection to mongoDB
    The connection was failing because of
    parameters passed in the function calling the mongoose
    these functions affected the await functiona
    and this caused a disconnect

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,11 +1,9 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
+    await mongoose.connect(process.env.DATABASE_URL);
   } catch (error) {
     console.error(error);
   }
